3f999a6c22387da3e2e7df9903b9736e7b1dbd81
Dev Sharma <devsharma1683@gmail.com>
2025-06-07 01:03:42 +0530

    route protection based on authentication

---
+++ ai-ticket-assistance/routes/user.js
@@ -0,0 +1,14 @@
+import e from "express";
+import { getUsers, login, signup, logout, updateUser } from "../controllers/user.js"
+import { authenticate } from "../middlewares/auth.js";
+
+const router = e.Router()
+
+router.post("/update-user", authenticate, updateUser);
+router.get("/users", authenticate, getUsers)
+
+router.post("/signup", signup)
+router.post("/login", login)
+router.post("/logout", logout)
+
+export default router
