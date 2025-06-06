61cc443dabd6a4c7ce1c9fadae405b3e37dce5cd
Dev Sharma <devsharma1683@gmail.com>
2025-06-07 01:03:10 +0530

    inngest cleint setup and sign on function

---
+++ ai-ticket-assistance/inngest/function/on-signup.js
@@ -0,0 +1,39 @@
+import { inngest } from "../client";
+import User from "../../models/user"
+import { NonRetriableError } from "inngest";
+import { sendEmail } from "../../utils/mailer";
+
+export const onUserSignUp = inngest.createFunction(
+    {id: "on-user-signup", retries: 2},
+    {event: "user/signup"},
+    
+    async ({event, step}) => {
+        try {
+            const {email} = event.data;
+            const user = await step.run("get-user-email", async()=>{
+                const userObject = await User.findOne({email})
+                
+                if (!userObject) {
+                    throw new NonRetriableError("User no longer exists in are database");
+                }
+
+                return userObject;
+            })
+
+            await step.run("send-welcome-email", async()=>{
+                const subject = `Welcome to the app`
+                const message = `
+                    Hi,
+                    \n\n
+                    Thanks for signing up..!, Glad to have you onBoard!
+                `
+                await sendEmail(user.email, subject, message)
+            })
+
+            return {success:true};
+        } catch (error) {
+            console.error("❌Error in running steps", error.message);
+            return {success:false};
+        }
+    }
+)
